c575931a5bcd9c0d6de17add01434a6991780426
Sairion350 <76807368+Sairion350@users.noreply.github.com>
2021-08-17 20:06:51 -0500

    first

--- src/Papyrus/PapyrusActor.h
+++ src/Papyrus/PapyrusActor.h
@@ -3,6 +3,7 @@
 namespace PapyrusActor
 {
 	using VM = RE::BSScript::IVirtualMachine;
+	using EventResult = RE::BSEventNotifyControl;
 
 	std::vector<RE::TESNPC*> LookupRelationshipPartners(RE::StaticFunctionTag*, RE::Actor* a_actor, RE::BGSAssociationType* a_type)
 	{
@@ -91,6 +92,114 @@ namespace PapyrusActor
 		return a_base->GetVoiceType();
 	}
 
+
+
+
+	class OnHitEventHandler : public RE::BSTEventSink<RE::TESHitEvent>
+	{
+	public:
+		static OnHitEventHandler* GetSingleton()
+		{
+			static OnHitEventHandler singleton;
+			return &singleton;
+		}
+
+		virtual EventResult ProcessEvent(const RE::TESHitEvent* a_event, RE::BSTEventSource<RE::TESHitEvent>*)
+		{
+			if (!a_event)
+				return EventResult::kContinue;
+
+			
+
+			if (a_event->target){
+				auto target = a_event->target->As<RE::Actor>();
+
+				if (target){
+					
+
+
+					if (true){
+						logger::info("Hit");
+
+
+						//target->GetActorBase()->SetActorValue(RE::ActorValue::kHealth, 0.5);
+						//auto z = target->avStorage.modifiers[RE::ActorValue::kHealth];
+						target->healthModifiers.modifiers[RE::ACTOR_VALUE_MODIFIER::kDamage] = -50.0f;
+						
+						float z = target->healthModifiers.modifiers[RE::ACTOR_VALUE_MODIFIER::kDamage];
+
+						logger::info("Base: {}", target->GetBaseActorValue(RE::ActorValue::kHealth));
+						logger::info("Full: {}", target->GetActorValue(RE::ActorValue::kHealth));
+						logger::info("Thing: {}", z);
+					}
+				}
+			}
+
+			
+
+			return EventResult::kContinue;
+		}
+
+	private:
+		OnHitEventHandler() = default;
+		OnHitEventHandler(const OnHitEventHandler&) = delete;
+		OnHitEventHandler(OnHitEventHandler&&) = delete;
+		virtual ~OnHitEventHandler() = default;
+
+		OnHitEventHandler& operator=(const OnHitEventHandler&) = delete;
+		OnHitEventHandler& operator=(OnHitEventHandler&&) = delete;
+	};
+
+	class OnDeathEventHandler : public RE::BSTEventSink<RE::TESDeathEvent>
+	{
+	public:
+		static OnDeathEventHandler* GetSingleton()
+		{
+			static OnDeathEventHandler singleton;
+			return &singleton;
+		}
+
+		virtual EventResult ProcessEvent(const RE::TESDeathEvent* a_event, RE::BSTEventSource<RE::TESDeathEvent>*)
+		{
+			if (!a_event)
+				return EventResult::kContinue;
+
+			
+
+			if (a_event->actorDying){
+				auto target = a_event->actorDying->As<RE::Actor>();
+
+				if (target){
+					
+
+
+					if (true){
+						logger::info("ded");
+
+						//target->healthModifiers.modifiers[RE::ACTOR_VALUE_MODIFIER::kDamage] = -50.0f;
+						target->Resurrect(false, false);
+						
+					}
+				}
+			}
+
+			
+
+			return EventResult::kContinue;
+		}
+
+	private:
+		OnDeathEventHandler() = default;
+		OnDeathEventHandler(const OnDeathEventHandler&) = delete;
+		OnDeathEventHandler(OnDeathEventHandler&&) = delete;
+		virtual ~OnDeathEventHandler() = default;
+
+		OnDeathEventHandler& operator=(const OnDeathEventHandler&) = delete;
+		OnDeathEventHandler& operator=(OnDeathEventHandler&&) = delete;
+	};
+
+
+
 	bool Bind(VM* a_vm)
 	{
 		const auto obj = "OSANative"sv;
@@ -103,7 +212,12 @@ namespace PapyrusActor
 		BIND(GetRace, true);
 		BIND(GetVoiceType, true);
 
-
+		auto events = RE::ScriptEventSourceHolder::GetSingleton();
+		if (events) {
+			//events->AddEventSink(OnHitEventHandler::GetSingleton());
+			//events->AddEventSink(OnDeathEventHandler::GetSingleton());
+		}
+		
 		return true;
 	}
 }
